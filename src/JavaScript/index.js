96616a0c26ff68424ecc2168f5eadf6e9055a197
Abd1h <abdlh.lllh@gmail.com>
2022-09-30 15:32:52 +0300

    htmlBranchReady

--- src/JavaScript/index.js
+++ src/JavaScript/index.js
@@ -3,6 +3,8 @@ import './../scss/main.scss';
 
 // importing assets folder
 import heroBackGroundImg from '../assets/hero-backgroundImg.jpg';
+import mapImg from '../assets/map-img.jpg';
+
 import servicesImg1 from '../assets/services-imgs/build-health.jpg';
 import servicesImg2 from '../assets/services-imgs/build-muscle.jpg';
 import servicesImg3 from '../assets/services-imgs/build-skills.jpg';
@@ -11,4 +13,7 @@ import servicesImg4 from '../assets/services-imgs/build-strength.jpg';
 import memberFace1 from '../assets/members-quotes-imgs/testimonal-1.jpg';
 import memberFace2 from '../assets/members-quotes-imgs/testimonal-2.png';
 import memberFace3 from '../assets/members-quotes-imgs/tes.png';
+
+import tourBackground from '../assets/tour-imgs/gymTour-background.jpg';
+import tourimg from '../assets/tour-imgs/gymTourImg.jpg';
 import svgs from '../assets/icons.svg';
